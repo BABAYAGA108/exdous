48ecb7714a0de3fca0e8bcc1d9753b871dc58753
kalu honour <honourkalu216@gmail.com>
2025-05-15 15:52:06 +0100

    work

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig(async () => {
+  const tailwindcss = await import('@tailwindcss/vite').then(m => m.default)
+  
+  return {
+    plugins: [
+      tailwindcss(),
+      // other plugins
+    ]
+  }
+})
